c282832b10c84810a048cf740b111d40257b5e73
suman <sumankannath@gmail.com>
2023-11-16 17:18:42 +0530

    renamed

--- eduverse/client/src/App.jsx
+++ eduverse/client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import UserRoute from "./Routes/userRoute";
+import UserRoute from "./Routes/UserRoute";
 import AdminRoute from "./Routes/AdminRoute";
 import MentorRoute from "./Routes/MentorRoute";
 import AuthRoute from "./Routes/AuthRoute";
